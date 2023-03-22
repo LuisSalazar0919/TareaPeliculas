5b09a5674f8c6daed753fbd821c8a04488cb515f
LuisSalazar0919 <126519399+LuisSalazar0919@users.noreply.github.com>
2023-03-22 06:14:08 +0000

    fase final(creo)

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -13,9 +13,6 @@ namespace Pelicula
         private string director; 
 
 
-
-
-
         private List<Actor> actores = new List<Actor>();
 
         //Constructores
@@ -72,6 +69,25 @@ namespace Pelicula
           Console.WriteLine($"{Titulo} ({Año})");
 
         }
+        
+        public void AgregaActor(Actor actor)
+        {
+            actores.Add(actor);
+        }
+
+        public void ImprimeActores()
+        {
+            foreach (Actor persona in actores)
+            {
+                Console.WriteLine($"{persona.GetNombre()} ({persona.GetAño()})");
+            }
+
+        }
+
+        
+        
+        
+
 
 
     }
@@ -79,11 +95,43 @@ namespace Pelicula
     public class Actor
     {
         //Propiedades
+        public string Nombre;
+        public Int16 Año;
+       
 
         //Constructores
+        public Actor(){}
+        public Actor(string Nombre , Int16 Año)
+        {
+            this.Nombre=Nombre;
+            this.Año=Año;
+        }
+        
 
 
         //Métodos 
+
+        public string GetNombre()
+        {
+            return Nombre;
+        }
+
+        public void SetNombre(string Nombre)
+        {
+            this.Nombre = Nombre;
+        }
+
+        public Int16 GetAño()
+        {
+            return Año;
+        }
+
+        public void SetAño(Int16 Año)
+        {
+            this.Año = Año;
+        }
+        
+        
         public void Imprime()
         {
            //  Console.WriteLine($"{Nombre} ({Año})");
@@ -105,8 +153,27 @@ namespace Pelicula
             p1.SetAño(2022);
             p2.SetTitulo("goku vs broly");
             p2.SetAño(2018);
+            p1.AgregaActor(new Actor ("Bredan Fraser", 1968));
+            p1.AgregaActor(new Actor("Sadie Sink", 2002));
+            p2.AgregaActor(new Actor("Goku", 737));
+            p2.AgregaActor(new Actor("Vegeta", 732 ));
             Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño() );
             Console.WriteLine("{0}({1})", p2.GetTitulo(), p2.GetAño() );
+
+            p1.ImprimeActores();
+            p2.ImprimeActores();
+
+            List<Pelicula> peluculaschidas = new List<Pelicula>();
+            peluculaschidas.Add(new Pelicula("chucky", 1988 ));
+            peluculaschidas.Add(new Pelicula("chucky 2", 1990 ));
+            peluculaschidas.Add(new Pelicula("chucky 3", 1991 ));
+            peluculaschidas.Add(new Pelicula("La novia del chucky ", 1998 ));
+            peluculaschidas.Add(new Pelicula("El Hijo de Chucky", 2004 ));
+
+            foreach(Pelicula chuckys in peluculaschidas)
+            {
+                chuckys.Imprime();
+            }            
         }
     }
 }
